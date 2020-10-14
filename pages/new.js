68d9ebae3eaab03cd5bf4d2e3cac9e9cb4210006
cluck-mur <cluck.mur@outlook.jp>
2020-10-14 15:00:24 +0900

    url1のレッスンまで終わった。

---
+++ pages/new.js
@@ -0,0 +1,35 @@
+export default function New() {
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const target = e.target
+        const title = target.title.value
+        const content = target.content.value
+        console.log(title)
+        console.log(content) 
+    }
+
+    return(
+        <div>
+            <h1>メモ作成</h1>
+            <div>
+            <form onSubmit={handleSubmit}>
+            <div>
+                <div>
+                <label htmlFor="">タイトル</label>
+                </div>
+                <input type="text" name="title" placeholder="title"/>
+            </div>
+            <div>
+                <div>
+                <label htmlFor="">本文</label>
+                </div>
+                <textarea name="content" placeholder="content"/>
+            </div>
+            <div>
+                <input type="submit" value="submit"/>
+            </div>
+            </form>
+            </div>
+        </div>
+    )
+}
